a189159f4a2cb9a3a7372e0be7ee6ad2c69d856b
atha <amhanime147@gmail.com>
2024-12-10 12:04:53 +0700

    first commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 //   useNewUrlParser: true,
 //   useUnifiedTopology: true,
 // });
-mongoose.connect("mongodb://localhost:27017/restful_db", {
+mongoose.connect("mongodb+srv://admin:1234@cluster0.llybf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
